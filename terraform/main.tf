964497d49c97335dc256eee478199d30bff6e80e
Oksana-knu <116543948+Oksana-knu@users.noreply.github.com>
2023-11-27 00:46:23 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,54 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 # Configure the AWS provider
 provider "aws" {
-  region = "eu-west-1"
+  access_key = ${{AWS_ACCESS_KEY}}
+  secret_key = ${{AWS_SECRET_KEY}}
+  region     = "eu-central-1"
+}
+
+resource "aws_security_group" "web_app" {
+  name        = "web_app"
+  description = "security group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "web_app"
+  }
+}
+
+resource "aws_instance" "webapp_instance" {
+  ami           = "ami-0669b163befffbdfc"
+  instance_type = "t2.micro"
+  security_groups= ["web_app"]
+  key_name = var.key_name
+  tags = {
+    Name = "webapp_instance"
+  }
 }
