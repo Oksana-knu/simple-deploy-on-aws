1ee66be3790d00bb6a3ea7372c58961c15340c1f
Oksana-knu <116543948+Oksana-knu@users.noreply.github.com>
2023-11-30 18:33:58 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,8 +12,6 @@ provider "aws" {
   region     = "eu-central-1"
 }
 
-variable "REPOSITORY_URI" {}
-
 resource "aws_lightsail_container_service" "flask_application" {
   name = "flask-app"
   power = "nano"
