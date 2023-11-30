49bd37092329879c58d4d9fb873a6011a719e1ed
Oksana-knu <116543948+Oksana-knu@users.noreply.github.com>
2023-11-30 13:52:57 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_lightsail_container_service_deployment_version" "flask_app_deploym
   container {
     container_name = "flask-application"
 
-    image = "myimage_xxxxx:latest"
+    image = "flask_app_repository/my_image:latest"
     
     ports = {
       # Consistent with the port exposed by the Dockerfile and app.py
